8d0ff51f01a3d32c3a2c059b42e5a11010378523
Guillaume Guerin <guillaume.guerin@fromearthtolife.net>
2026-04-11 20:18:55 +0200

    Add Google Tag Manager integration to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
+const GTM_ID = "GTM-M2GSG3V4";
+
 // Dépôt GitHub Pages : https://<user>.github.io/miss-contraction/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   base: "/miss-contraction/",
   plugins: [
+    {
+      name: "google-tag-manager",
+      transformIndexHtml() {
+        if (command !== "build") return [];
+        return [
+          {
+            tag: "script",
+            injectTo: "head",
+            children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
+          },
+          {
+            tag: "noscript",
+            injectTo: "body-prepend",
+            children: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          },
+        ];
+      },
+    },
     {
       name: "miss-contraction-trailing-slash",
       configureServer(server) {
@@ -123,4 +143,4 @@ export default defineConfig({
       },
     }),
   ],
-});
+}));
